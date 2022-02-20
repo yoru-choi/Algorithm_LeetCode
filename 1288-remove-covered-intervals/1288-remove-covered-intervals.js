3f498036c525d24b77c65dc9261d41aea7529650
Ten <qlqjs674@naver.com>
2022-02-20 15:55:10 +0900

    Time: 4078 ms (5.26%), Space: 59 MB (5.26%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+    
+    let arr = intervals; 
+    
+    while(1){
+        let len = arr.length;
+        arr = arr.sort();
+        
+        let num = 0;
+        let end = true;
+        let i =0
+        let j =0
+
+         for(i; i<len; i++ ){
+            if(!end)break;
+            j =0
+            for(j; j< len; j++){
+                
+               if(!end)break;
+                if(i != j){
+                    if(arr[i][0] <= arr[j][0]){
+              
+                        if(arr[i][1] >= arr[j][1]){
+
+                            arr.splice(j, 1); 
+                            end= false;
+                            continue;
+                            
+                            
+                        }
+                    }
+                }
+               
+
+            }
+             if(end)num++
+        }
+        
+        
+        if(len == num){
+           if(end){
+               return len;
+           }
+        }
+    }
+    
+};
