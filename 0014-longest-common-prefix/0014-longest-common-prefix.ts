02be73c939ca2237d21e747729795faa8c9e1f90
YEOL <qlqjs674@naver.com>
2022-10-16 16:55:46 +0900

    Time: 78 ms (89.43%), Space: 44.5 MB (62.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.ts
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -7,10 +7,10 @@ function longestCommonPrefix(strs: string[]): string {
     const strLen = strs.length
     if(strLen == 0)return "" //arr 0 @case1
     if(strLen == 1)return strs[0] //arr 1 @case2 
-
+    
     strs.sort();
     for (let i = 0; i < strs[0].length; i++) {
-    if(strs[0][i] !== strs[strs.length-1][i])return strs[0].substr(0, i); // @case3
+    if(strs[0][i] !== strs[strLen-1][i])return strs[0].substr(0, i); // @case3
     }
     return strs[0];//all same @case4
 };
