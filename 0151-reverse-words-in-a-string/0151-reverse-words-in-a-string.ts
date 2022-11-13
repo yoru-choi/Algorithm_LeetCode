002eb6dfeae58cf19e8b77527019b3c9588fa24c
YEOL <qlqjs674@naver.com>
2022-11-13 11:51:02 +0900

    Time: 133 ms (14.54%), Space: 49.3 MB (5.45%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -0,0 +1,10 @@
+
+function reverseWords(s: string): string {
+    const strTrimmed:string = s.trim()
+    if(strTrimmed.length <=0) return ''
+    const strLastIndex:number = strTrimmed.lastIndexOf(' ');
+    const getWord:string = strTrimmed.substring(strLastIndex)
+    const remainSentence:string = strTrimmed.substring(0, strLastIndex)
+    return (getWord+' ' + (remainSentence ? reverseWords(remainSentence) : '' )).trim()
+ 
+ };
